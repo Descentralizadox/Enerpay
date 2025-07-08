42d2cd741baefab998e02335495c1265eb2fc076
Descentralizadox <jovtec001@gmail.com>
2025-07-08 13:40:36 -0600

    🔧 Corregir imports React innecesarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import BottomNavigation from './components/BottomNavigation';
 import Footer from './components/Footer';
